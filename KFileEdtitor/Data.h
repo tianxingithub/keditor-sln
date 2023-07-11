e924d928f2c0201c9cf05800952675b6cf40736c
tianxingithub <108486677+tianxingithub@users.noreply.github.com>
2023-07-11 10:14:41 +0800

    1.3 已完成读取文件

--- KFileEdtitor/Data.h
+++ KFileEdtitor/Data.h
@@ -5,11 +5,14 @@
 //! 树节点的键值和其属性值
 class Data
 {
+public:
 	//! 树节点的键值
 	QMap<QString , QMap<QString , QString >*>* rootMap;
 
 	//! 树节点属性的顺序
 	QMap<QString , QList<QString >*>* order;
 
+public:
+	//<QString, QMap<QString, QString >*>* getRootMap
 };
 
--- KFileEdtitor/ReadWrite.cpp
+++ KFileEdtitor/ReadWrite.cpp
@@ -1,5 +1,6 @@
 #include "ReadWrite.h"
 #include "QFile"
+#include "qtextstream.h"
 
 ReadWrite::ReadWrite()
 {
@@ -16,9 +17,65 @@ Data* ReadWrite::readData(QString filepath)
 	Data* re = new Data();
 	QFile file(filepath);
 	if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-		return;
+		return nullptr;
 
+	QTextStream in(&file);
+	QString kItem; //! 上一个选项卡名字
+	QMap<QString, QString>* itemMap; //! 节点的属性信息
+	QList<QString>* itemOrder; //! 节点属性的顺序
 
+	//! 按行读取文件
+	while (!in.atEnd()) 
+	{
+		QByteArray line = file.readLine();
+		QString str(line);
+		str.remove("\n");
+
+		//! 将内容显示到QText上面
+		//! 显示所有内容还是可只显示选项卡内容
+
+		str = str.simplified();
+		//! 判断选项卡
+		if (str.at(0) == '*') 
+		{
+			if (str == "*NODE" || str == "*ELEMENT_SOLID" || str == "*KEYWORD" || str == "*PARAMETER_DUPLICATION" || str == "*END")
+				continue;
+			kItem = str;
+			itemMap = new QMap<QString, QString>();
+			itemOrder = new QList<QString>();
+		}
+		//! 添加选项卡属性值
+		else if (str.at(0) == '$' && str.at(1) != '#') 
+		{
+			QStringList key = str.split(" "); // 下标1开始，最后一个为unused要丢弃
+			int len = key.length();
+			if (key[len - 1].mid(0, 6) == "unused")
+			{
+				--len;
+			}
+			
+			line = file.readLine(); // 属性的值 下标0开始
+			QString strvalue(line);
+			strvalue.remove("\n");
+			strvalue = strvalue.simplified();
+			QStringList value = strvalue.split(" "); // 下标0开始
+
+			for (int i = 0; i < len - 1; i++)
+			{
+				QT_TRY{
+					itemMap->insert(key[i + 1], value[i]);
+					itemOrder->append(key[i + 1]);
+				}
+				QT_CATCH(...)
+				{
+
+				}
+			}
+			re->rootMap->insert(kItem, itemMap);
+		}
+	}
+
+	file.close();
 
 	return re;
 }
